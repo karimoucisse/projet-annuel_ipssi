e49f63d8472fb1bd8fdc24a8b83ff57064c92f96
karimou cisse <karimou@pylote.io>
2023-04-21 16:39:18 +0200

    [ADD] mooge connection

--- api/app.js
+++ api/app.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const express = require('express')
+const { connectToMongodb } = require('./config/database')
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
+
+connectToMongodb()
 
 app.use(() => {
     console.log(`Serveur is running on port ${PORT}`)
