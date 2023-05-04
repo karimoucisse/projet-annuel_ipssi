d65d85068e8e7702f655c2d94b2a9557414abcc6
Chérif BELLAHOUEL <cherifb@MacBook-Pro-de-Cherif.local>
2023-05-04 16:32:25 +0200

    Add schemas

---
+++ api/models/address.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const addressSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    wayType: {
+        type: Number,
+        required: true,
+    },
+    number: {
+        type: Number,
+        required: true,
+    },
+    addressName: {
+        type: String,
+        required: true,
+    },
+    postalCode: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+});
+
+const Address = mongoose.model('File', addressSchema);
+
+module.exports = Address;
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,17 +2,36 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
+const userSchema = new Schema(
+    {
+        firstname: {
+            type: String,
+            required: true,
+        },
+        lastname: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+        },
+        role: {
+            type: String,
+            required: true,
+            default: 'USER',
+        },
     },
-    password: {
-        type: String,
-        required: true,
-    },
-});
+    { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
