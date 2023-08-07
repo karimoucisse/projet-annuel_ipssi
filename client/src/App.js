ef3350849d9370edeee8e3bace929b77cca6f368
karimou cisse <karimou@pylote.io>
2023-08-07 10:00:47 +0200

    change: frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
-import Router from './router/router';
-
-/**
- * The function exports a component called "App" that returns a "Routeur" component.
- * @returns A component called "Routeur" is being returned.
- */
-export default function App() {
-  return <Router />;
+function App() {
+  return <div>hello world !</div>;
 }
+
+export default App;
