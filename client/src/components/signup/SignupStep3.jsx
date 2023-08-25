c8f4963edcc47e3bf362d9de7481f51e3733f28b
karimou cisse <karimou@pylote.io>
2023-08-25 18:47:48 +0200

    add: component SignupStep3, payment page

---
+++ client/src/components/signup/SignupStep3.jsx
@@ -0,0 +1,5 @@
+const SignupStep3 = ({ setStep }) => {
+  return <div>SignupStep3</div>;
+};
+
+export default SignupStep3;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -26,11 +26,13 @@ import SignupStep1 from '../components/signup/SignupStep1';
 import SignupStep2 from '../components/signup/SignupStep2';
 import zIndex from '@mui/material/styles/zIndex';
 import StepperSection from '../components/StepperSection';
+import SignupStep3 from '../components/signup/SignupStep3';
 const Signup = () => {
   const [step, setStep] = useState(1);
   const tab = {
     step1: <SignupStep1 setStep={setStep} />,
     step2: <SignupStep2 setStep={setStep} />,
+    step3: <SignupStep3 setStep={setStep} />,
   };
 
   const stepsValue = ['Inscription', 'adresse de paiment', 'paiment'];
