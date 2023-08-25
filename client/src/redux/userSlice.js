20f51403f92b40b440f55720e6b74693c7248d2e
karimou cisse <karimou@pylote.io>
2023-08-25 12:05:39 +0200

    add: folder ../client/src/redux/ and ../.next/

---
+++ client/src/redux/userSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  },
+});
