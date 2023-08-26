160beeab578e5866a8b2b92cd9eac0b4e5b10780
karimou cisse <karimou@pylote.io>
2023-08-26 15:08:47 +0200

    add: mui global theme folder, add: ThemeProvider in router.js

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -2,15 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from '../pages/Signup';
 import Signin from '../pages/Signin';
 import Home from '../pages/Home';
+import { ThemeProvider } from '@mui/material';
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+          </Routes>
+        </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
