be52acd013dba1036e392228907d30a056a895cb
karimou cisse <karimou@pylote.io>
2023-10-26 23:45:55 +0200

    fix: react file naming, add: redux folder

--- client/src/components/auth/ArchiConnectContainer.jsx
+++ client/src/components/auth/ArchiConnectContainer.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
 const ArchiConnectContainer = () => {
-    return (
-        <Box
-            flex={0.9}
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                padding: 4,
-                gap: 3,
-                backgroundColor: '#1c2930',
-                color: 'white',
-            }}
-        >
-            <Typography variant="h2" textAlign="center" width="100%">
-                ArchiConnect
-            </Typography>
-            <Typography variant="subtitle1">
-                Conservez vos fichiers en toute sécurité, prêts à <br /> être
-                consultés et partagés à tout moment.
-            </Typography>
-        </Box>
-    );
+  return (
+    <Box
+      flex={0.9}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 4,
+        gap: 3,
+        backgroundColor: "#1c2930",
+        color: "white",
+      }}
+    >
+      <Typography variant="h2" textAlign="center" width="100%">
+        ArchiConnect
+      </Typography>
+      <Typography variant="subtitle1">
+        Conservez vos fichiers en toute sécurité, prêts à <br /> être consultés
+        et partagés à tout moment.
+      </Typography>
+    </Box>
+  );
 };
 
 export default ArchiConnectContainer;
--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,59 +1,84 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NoMatch from './NoMatch';
-import Container from '../layouts/container';
-import Main from '../pages/main';
-import AuthRouter from '../pages/auth/authRouter';
-import AuthGuard from '../_helpers/authGuard';
-import AdminRouter from '../pages/admin/adminRouter';
-import Details from '../pages/details';
-import CheckoutSuccess from '../pages/stripe/checkoutSuccess';
-import CheckoutFailed from '../pages/stripe/checkoutFailed';
-import VisitorGuard from '../_helpers/visitorGuard';
-import MyDocument from '../pages/profile/storage';
-import ProfileRouter from '../pages/profile/profileRouter';
-import LegalInformation from '../pages/InformationsLegal';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NoMatch from "./NoMatch";
+import GlobalContainer from "../layouts/GlobalContainer";
+import Main from "../pages/main";
+import AuthGuard from "../_helpers/authGuard";
+import AdminRouter from "../pages/admin/adminRouter";
+import Details from "../pages/details";
+import CheckoutSuccess from "../pages/stripe/checkoutSuccess";
+import CheckoutFailed from "../pages/stripe/checkoutFailed";
+import VisitorGuard from "../_helpers/visitorGuard";
+import MyDocument from "../pages/profile/storage";
+import ProfileRouter from "../pages/profile/profileRouter";
+import LegalInformation from "../pages/InformationsLegal";
+import Authentication from "../pages/auth/authentication";
+import Subscription from "../pages/auth/subscription";
 
 const Routeur = () => {
   return (
     <BrowserRouter>
-      <Container>
+      <GlobalContainer>
         <Routes>
-          <Route path={"/"} element={
-            <AuthGuard>
-              <Main />
-            </AuthGuard>
-          } />
-          <Route path={"/details/:fileId"} element={
-            <AuthGuard>
-              <Details />
-            </AuthGuard>
-          } />
-          <Route path="/admin/*" element={ // TODO: FAIRE AUTH ADMIN GUARD POUR SECURISER LES ROUTES ADMIN
-            <AuthGuard>
-              <AdminRouter />
-            </AuthGuard>
-          } />
-          <Route path="/profile/*" element={
-            <AuthGuard>
-              <ProfileRouter />
-            </AuthGuard>
-          } />
-          <Route path="/storage/" element={
-            <AuthGuard>
-              <MyDocument />
-            </AuthGuard>
-          } />
+          <Route
+            path={"/"}
+            element={
+              <AuthGuard>
+                <Main />
+              </AuthGuard>
+            }
+          />
+          <Route
+            path={"/details/:fileId"}
+            element={
+              <AuthGuard>
+                <Details />
+              </AuthGuard>
+            }
+          />
+          <Route
+            path="/admin/*"
+            element={
+              // TODO: FAIRE AUTH ADMIN GUARD POUR SECURISER LES ROUTES ADMIN
+              <AuthGuard>
+                <AdminRouter />
+              </AuthGuard>
+            }
+          />
+          <Route
+            path="/profile/*"
+            element={
+              <AuthGuard>
+                <ProfileRouter />
+              </AuthGuard>
+            }
+          />
+          <Route
+            path="/storage/"
+            element={
+              <AuthGuard>
+                <MyDocument />
+              </AuthGuard>
+            }
+          />
           <Route path="/informations-legales" element={<LegalInformation />} />
           <Route path="/checkout-success" element={<CheckoutSuccess />} />
           <Route path="/checkout-failed" element={<CheckoutFailed />} />
-          <Route path="/auth/*" element={
-            <VisitorGuard>
-              <AuthRouter />
-            </VisitorGuard>
-          } />
+          <Route
+            path="/auth/*"
+            element={
+              <VisitorGuard>
+                <Routes>
+                  <Route index element={<Authentication />} />
+                  <Route path="login" element={<Authentication />} />
+                  <Route path="signup" element={<Subscription />} />
+                  <Route path="*" element={<Authentication />} />
+                </Routes>
+              </VisitorGuard>
+            }
+          />
           <Route path="*" element={<NoMatch />} />
         </Routes>
-      </Container>
+      </GlobalContainer>
     </BrowserRouter>
   );
 };
